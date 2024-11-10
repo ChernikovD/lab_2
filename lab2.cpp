992c34a42c7a6700cc48d07015e41d04adc1e483
Danil Chernikov <chernikovdanil@gmail.com>
2024-11-10 12:14:00 +0700

    Created structures

--- lab2.cpp
+++ lab2.cpp
@@ -1,20 +1,47 @@
-﻿// lab2.cpp : Этот файл содержит функцию "main". Здесь начинается и заканчивается выполнение программы.
-//
+﻿#include <iostream>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 
-#include <iostream>
+#define MAXIMUM 100
+
+typedef struct {
+    char firstName[MAXIMUM];
+    char lastName[MAXIMUM];
+} Author;
+
+typedef struct {
+    char title[MAXIMUM];
+    Author author;
+    int year;
+    int pages;
+} Book;
+
+typedef struct {
+    char firstName[MAXIMUM];
+    char lastName[MAXIMUM];
+    int readerID;
+} Reader;
+
+typedef struct {
+    Book book;
+    Reader reader;
+    char orderDate[MAXIMUM];
+    char returnDate[MAXIMUM];
+} Order;
+
+typedef struct {
+    char name[MAXIMUM];
+    char address[MAXIMUM];
+    Book books[MAXIMUM];
+    Reader readers[MAXIMUM];
+    Order orders[MAXIMUM];
+    int bookCount;
+    int readerCount;
+    int orderCount;
+} Library;
 
 int main()
 {
-    std::cout << "Hello World!\n";
+    return 0;
 }
-
-// Запуск программы: CTRL+F5 или меню "Отладка" > "Запуск без отладки"
-// Отладка программы: F5 или меню "Отладка" > "Запустить отладку"
-
-// Советы по началу работы 
-//   1. В окне обозревателя решений можно добавлять файлы и управлять ими.
-//   2. В окне Team Explorer можно подключиться к системе управления версиями.
-//   3. В окне "Выходные данные" можно просматривать выходные данные сборки и другие сообщения.
-//   4. В окне "Список ошибок" можно просматривать ошибки.
-//   5. Последовательно выберите пункты меню "Проект" > "Добавить новый элемент", чтобы создать файлы кода, или "Проект" > "Добавить существующий элемент", чтобы добавить в проект существующие файлы кода.
-//   6. Чтобы снова открыть этот проект позже, выберите пункты меню "Файл" > "Открыть" > "Проект" и выберите SLN-файл.
